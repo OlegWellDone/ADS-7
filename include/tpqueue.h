5b6d3cbedfc420206e55d771339362243ae0125b
OlegWellDone <63550097+OlegWellDone@users.noreply.github.com>
2022-05-01 15:23:53 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,6 +1,7 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T>
 class TPQueue {
@@ -27,16 +28,16 @@ void TPQueue <T> :: push(const T&) {
     head = new ITEM;
     head -> value = value;
     head -> next = nullptr;
-   //присваивание следующему элементу нуля, чтобы существовала ссылка на следующий будущий элемент
+//присваивание чтобы существовала ссылка на следующий будущий элемент
   } else if (value.prior > head -> value.prior) {
-   //случай если полученный приоритет выше чем приоритет который у элемента головы
+//случай если полученный приоритет выше чем приоритет который у элемента головы
     ITEM * temp = new ITEM;
     temp -> value = value;
     temp -> next = head;
     head = temp;
   } else {
     ITEM * prop = head;
-    while (prop -> hext) {
+    while (prop -> next) {
       if (value.prior > prop -> next -> value.prior) {
         ITEM * temp = new ITEM;
         temp -> value = value;
